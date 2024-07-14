e9990a1e0dd44b31b277659456f8549763d5d4cd
Ayush0to1 <136285934+Ayush0to1@users.noreply.github.com>
2024-07-14 02:15:27 +0000

    container cofig

--- JS/First.js
+++ JS/First.js
@@ -1,1 +1,14 @@
+//Node Js.
 console.log("Started")
+//Learning about variable, constant.
+const accountId=13711           // It's constant that is defined by "const"
+var accountName="User_TD"       //It's a variable thta can be defined by "var","let",or nothing.
+let accountType="Public"        //It's used as variable in most cases. because it support scope. i.e:{}
+accountPassword="HuhuHooooo"
+// Remember "const" can not be changed
+//changing the variable name here.
+accountName="Tiger chi"
+console.table([accountId, accountName, accountType, accountPassword]);
+
+// In order to display the output we should use "node.(file_name.js)"
+// If the file is in other folder then we should use "node (folder_name)/(file_name.js)"
