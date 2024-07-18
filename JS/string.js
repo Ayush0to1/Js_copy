36bfb71db960b933871fef54ab049537e6215426
Ayush0to1 <136285934+Ayush0to1@users.noreply.github.com>
2024-07-18 08:11:36 +0000

    string

---
+++ JS/string.js
@@ -0,0 +1,18 @@
+const name="acer";
+price=120000
+console.log(`The one of best laptop ${name} costes is around ${price}`)         // This is used in modern time.
+//New way to decleare string
+const brand=new String('Dell')                          //It is object
+console.log(typeof name) 
+console.log(typeof brand)
+
+// Notes we can get obtain more information. if we go to 'console' and write the code of 5 line.
+console.log(brand.toUpperCase())
+console.log(brand)
+// like wise we can use
+// .slice(star_point, end_point)    to make substring, we can also give negative value to print in reverse order.
+// .substring(1,7) 
+// .trim()  is used to delete/remove space in string.
+// .replace('replace_string','by_this')
+// .includes('Is_this_value_is_given_in_string')    answer true/false.
+// .split('based on what')          It breaks the string 
